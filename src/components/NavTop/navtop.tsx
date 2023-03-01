370c5c50ccb99aca311e959ba27c240697067d21
Rossinho <matheusbprossi@gmail.com>
2023-03-01 16:55:47 -0300

    responsividade da navbar corrigida

--- src/components/NavTop/navtop.tsx
+++ src/components/NavTop/navtop.tsx
@@ -2,73 +2,81 @@ import { useEffect, useRef, useState } from "react";
 import "./navtop.css"
 
 export function NavTop(){
-    const [navItemsActive, setNavItemsActive] = useState<boolean>(false)
-    const [currentNav, setCurrentNav] = useState<string | null>('main');
-    const navRef = useRef<HTMLUListElement>(null);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        const sections = document.querySelectorAll("section");
-  
-        sections.forEach((section) => {
-          const sectionTop = section.offsetTop;
-          if (window.scrollY >= sectionTop - 70.39) {
-            setCurrentNav(section.getAttribute("id"));
-          }
-        });
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    useEffect(() => {
-      const navLi = navRef.current?.querySelectorAll('a');
-  
-      navLi?.forEach((li) => {
-        li.classList.remove('active');
-        if (currentNav && li.classList.contains(currentNav)) {
-          li.classList.add('active');
+    const [navItemsActive, setNavItemsActive] = useState<boolean>(false);
+  const [currentNav, setCurrentNav] = useState<string | null>("main");
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const navRef = useRef<HTMLUListElement>(null);
+
+  const handleCloseNav = () => {
+    if (navItemsActive) {
+      setNavItemsActive(false);
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, scrollPosition);
+    }
+  };
+
+  useEffect(() => {
+    if (navItemsActive) {
+      const scrollY = window.scrollY;
+      setScrollPosition(scrollY);
+      document.body.style.position = "fixed";
+      document.body.style.width = "100vw";
+      window.scrollTo(0, 0);
+    } else {
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, scrollPosition);
+    }
+  }, [navItemsActive]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (window.scrollY >= sectionTop - 70.39) {
+          setCurrentNav(section.getAttribute("id"));
         }
       });
-    }, [currentNav]);
+    };
 
-    useEffect(()=>{
-        if(navItemsActive){
-            console.log(window.scrollY);
-            document.body.style.top = `-${window?.scrollY}px`
-            document.body.style.position = "fixed";
-            
-            
-        }
-        else{
-            const scrollY = document.body.style.top;
-            document.body.style.position = '';
-            document.body.style.top = '';
-            window.scrollTo(0, parseInt(scrollY || '0') * -1);
-        }
-    },[navItemsActive])
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const navLi = navRef.current?.querySelectorAll("a");
+
+    navLi?.forEach((li) => {
+      li.classList.remove("active");
+      if (currentNav && li.classList.contains(currentNav)) {
+        li.classList.add("active");
+      }
+    });
+  }, [currentNav]);
 
     return(
         <div className={
             `sticky-top navtop d-flex justify-content-between py-3 align-items-md-center
-            align-items-start px-4 px-md-5
+            align-items-start px-4 px-md-5 w-100
             `}>
-            <div className="matheus d-flex flex-row align-items-center">
-                <a style={{cursor:'pointer'}} href="#main"><h2 className="mb-0"><span>Matheus</span> Rossi</h2></a>
+            <div className="matheus d-flex flex-row align-items-center justify-content-between w-100">
+                <a style={{cursor:'pointer'}} href="#main" className="title"><h2 className="mb-0"><span>Matheus</span> Rossi</h2></a>
                 
-            </div>
-            <nav className="d-md-block d-flex justify-content-center flex-column align-items-center" >
-            <button className={`d-md-none text-center`} onClick={()=>setNavItemsActive(!navItemsActive)}>
+            <button className={`d-md-none text-center`} >
 
                 {navItemsActive ? 
-                <i className="fa fa-times fa-2xl mt-3"></i>
-                : <i className="fa fa-bars fa-xl mt-3"></i>  
+                <i className="fa fa-times fa-2xl mt-3" onClick={handleCloseNav}></i>
+                : <i className="fa fa-bars fa-xl mt-3" onClick={(()=>{setNavItemsActive(true)})}></i>  
                 }
             </button>
+            </div>
+            <nav className="d-md-block d-flex justify-content-center flex-column align-items-center" >
             <ul className={`navtop-items d-md-flex align-items-md-center mb-md-0 d-flex px-4 px-md-0 mt-0 flex-row
             
             ${navItemsActive ? 
